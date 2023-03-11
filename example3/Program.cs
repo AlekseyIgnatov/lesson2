17add68703dc82f28c9fb19daa9db026d046444b
Alex <alex.ignatov@mail.ru>
2023-03-11 17:39:13 +0300

    Находим максимальное из трех чисел

--- example3/Program.cs
+++ example3/Program.cs
@@ -6,6 +6,7 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = 0;
+
 if (number1 >= number2){max = number1;}
     else {max = number2;}
 if (max >= number3){Console.WriteLine($" Максимальное число равно {max} ");}
