dd1f07d93922bfaf745d1beae1c0e5c8ddc8cb16
Alex <alex.ignatov@mail.ru>
2023-03-28 16:36:20 +0300

    Считаем количество чисел > 0 в массиве размером N

---
+++ example19/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void FillArray(int[] array)
+    {
+       for (int i=0; i<array.Length; i++) 
+            {  //array[i] = new Random().Next(-100, 101);
+            Console.Write("Введите {0}-е число: ",i+1);
+            array[i] = Convert.ToInt32(Console.ReadLine());
+            };
+    }
+
+void PrintArray(int[] array)
+    {
+       foreach (int item in array)
+            {
+                Console.Write($"{item} ");
+            }
+    }
+
+int NPositive(int[] array)
+    {
+        int count =0;
+        for (int i=0; i < array.Length; i++)
+            if (array[i] > 0) count++;
+        return count;
+    }
+
+
+Console.Write($"Введите количество чисел ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("");
+Console.WriteLine($"Колличество чисел больше нуля: {NPositive(array)}");
