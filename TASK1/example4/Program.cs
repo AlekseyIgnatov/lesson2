49b15d6caed97360ed3c3286c46b74e0eff382b8
Alex <alex.ignatov@mail.ru>
2023-03-30 14:05:36 +0300

    Домашнее задание к первому семинару

---
+++ TASK1/example4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (number1 >= number2){max = number1;}
+    else {max = number2;}
+if (max >= number3){Console.WriteLine($" Максимальное число равно {max} ");}
+    else {
+        max = number3;
+        Console.WriteLine($" Максимальное число равно {max} ");
+        };
