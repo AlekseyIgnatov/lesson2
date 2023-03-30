c34f37c82f2a5120da5a4be0512900be1467aa2b
Alex <alex.ignatov@mail.ru>
2023-03-30 14:08:02 +0300

    Домашнее задание к четвертому семинару

---
+++ TASK4/example29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] arr = new int[8];
+
+void FillArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(0,100);
+}
+
+void PrintArr(int[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]},");
+}
+
+FillArr(arr);
+PrintArr(arr);
